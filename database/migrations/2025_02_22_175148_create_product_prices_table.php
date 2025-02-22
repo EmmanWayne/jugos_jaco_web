2ecc814436ee902388e195d11fd88e64546b3ddc
Cristian Emmanuel Díaz Rodríguez <143372290+EmmanWayne@users.noreply.github.com>
2025-02-22 11:56:54 -0600

    Creación del modelo y migraciones de los precios de los productos
    Se creo el modelo y las migraciones de la tabla products_prices

---
+++ database/migrations/2025_02_22_175148_create_product_prices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('type_price_id')->constrained('types_prices');
+            $table->foreignId('product_id')->constrained('products');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_prices');
+    }
+};
