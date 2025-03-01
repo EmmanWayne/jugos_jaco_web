30b62706850d61df5f7ddfbf62ec926d83dcaba4
abdias98 <abdiasv32@gmail.com>
2025-02-28 19:51:50 -0600

    feat: edicion de migracion de usuarios

--- database/migrations/2025_02_20_160001_create_users_table.php
+++ database/migrations/2025_02_20_160001_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 32);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('employed_id')->unique();
+            $table->boolean('status')->default(true);
+
+            $table->foreignId('employed_id')->constrained('employees')->restrictOnUpdate();
             $table->rememberToken();
             $table->timestamps();
         });
