c6f7deb8c2a27cf6de63cbcc33efcb5c27e2f9b7
abdias98 <abdiasv32@gmail.com>
2025-03-01 03:07:44 -0600

    feat: API para la gestion de clientes

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use App\Models\Location;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+class ClientController extends Controller
+{
+    /**
+     * Metodo encargado de obtener los clientes
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getClients()
+    {
+        try {
+            $clients = Client::where('employee_id', Auth::user()->id)
+                ->with(['location:model_id,latitude,longitude', 'typePrice:id,name'])
+                ->get();
+
+            return response()->json([
+                'clients' => $clients
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Error al obtener los clientes.',
+                'error' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 500);
+        }
+    }
+
+    /**
+     * Metodo encargado de crear un cliente
+     * 
+     * @param Request $request
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function createClient(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $request->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'address' => 'required|string',
+                'phone_number' => 'required|string',
+                'department' => 'required|string',
+                'township' => 'required|string',
+                'latitude' => 'required|string',
+                'longitude' => 'required|string',
+            ]);
+
+            $this->validateExistingClient($request);
+
+            $client = new Client();
+            $client->first_name = $request->first_name;
+            $client->last_name = $request->last_name;
+            $client->address = $request->address;
+            $client->phone_number = $request->phone_number;
+            $client->department = $request->department;
+            $client->township = $request->township;
+            $client->employee_id = Auth::user()->id;
+            $client->save();
+
+            $location = new Location();
+            $location->latitude = $request->latitude;
+            $location->longitude = $request->longitude;
+            $client->location()->save($location);
+
+            DB::commit();
+
+            $client->load(['location:model_id,latitude,longitude', 'typePrice:id,name']);
+
+            return response()->json([
+                'message' => 'Cliente creado correctamente.',
+                'client' => $client
+            ]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Error al crear el cliente.',
+                'error' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 500);
+        }
+    }
+
+    /**
+     * Metodo encargado de actualizar un cliente
+     * 
+     * @param Request $request
+     * @param int $id
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function updateClient(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+            $client = Client::find($id);
+
+            if (!$client) {
+                throw ValidationException::withMessages([
+                    'client' => 'Cliente no encontrado.'
+                ]);
+            }
+
+            $this->validateExistingClient($request, $id);
+
+            $request->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'address' => 'required|string',
+                'phone_number' => 'required|string',
+                'department' => 'required|string',
+                'township' => 'required|string',
+                'latitude' => 'required|string',
+                'longitude' => 'required|string',
+            ]);
+
+            $client->first_name = $request->first_name;
+            $client->last_name = $request->last_name;
+            $client->address = $request->address;
+            $client->phone_number = $request->phone_number;
+            $client->department = $request->department;
+            $client->township = $request->township;
+            $client->save();
+
+            $location = $client->location;
+            $location->latitude = $request->latitude;
+            $location->longitude = $request->longitude;
+            $location->save();
+
+            $client->load(['location:model_id,latitude,longitude', 'typePrice:id,name']);
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Cliente actualizado correctamente.',
+                'client' => $client
+            ]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Error al actualizar el cliente.',
+                'error' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 500);
+        }
+    }
+
+    /**
+     * Metodo encargado de validar si ya existe un cliente con el mismo nombre y número telefónico
+     */
+    private function validateExistingClient($request, $client_id = null)
+    {
+        $existingClient = Client::when($client_id, function ($query) use ($client_id) {
+                $query->where('id', '!=', $client_id);
+            })
+            ->where([['last_name', $request->last_name], ['first_name', $request->first_name], ['phone_number', $request->phone_number], ['employee_id', Auth::user()->id]])
+            ->first();
+
+        if ($existingClient) {
+            throw ValidationException::withMessages([
+                'message' => 'Ya existe un cliente con el mismo nombre y número telefónico.'
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-}); 
+
+    Route::prefix('clients')->group(function () {
+        Route::get('/', [ClientController::class, 'getClients']);
+        Route::post('/', [ClientController::class, 'createClient']);
+        Route::put('/{id}', [ClientController::class, 'updateClient']);
+    });
+});
