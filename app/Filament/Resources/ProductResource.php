96b4255bb211a6a3e98c821a1a1841c4fb145465
Cristian Emmanuel Díaz Rodríguez <143372290+EmmanWayne@users.noreply.github.com>
2025-03-26 15:54:13 -0600

    Creacion de crud de las categorias de los productos

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -26,6 +26,8 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Productos';
+
     public static function form(Form $form): Form
     {
         return $form
