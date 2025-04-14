3538429d58b56569b46ace1c7edf6f396efd57f7
abdias98 <abdiasv32@gmail.com>
2025-04-14 00:10:33 -0600

    feat: recursos filament para la creacion de depositos

---
+++ app/Filament/Resources/DepositResource/Pages/EditDeposit.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\DepositResource\Pages;
+
+use App\Filament\Resources\DepositResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDeposit extends EditRecord
+{
+    protected static string $resource = DepositResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->record]);
+    }
+}
