fe1ad1c77f76ca7531aabfe4e6c21ebd40eb6ede
abdias98 <abdiasv32@gmail.com>
2025-04-14 16:40:45 -0600

    feat: recurso filamente para la creacion, eliminacion y edicion del inventario de productos terminados

---
+++ app/Filament/Resources/FinishedProductInventoryResource/Pages/CreateFinishedProductInventory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\FinishedProductInventoryResource\Pages;
+
+use App\Filament\Resources\FinishedProductInventoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFinishedProductInventory extends CreateRecord
+{
+    protected static string $resource = FinishedProductInventoryResource::class;
+}
